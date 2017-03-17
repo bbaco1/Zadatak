397be131e7f1aad6294ccc0b2bd202d8428ac79d
Milorad Orzes <miloradorzes@milorads-mac.lan>
2017-03-17 14:24:22 +0100

    added missing files

--- Zadatak/RepositoryViewController.m
+++ Zadatak/RepositoryViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "RepositoryViewController.h"
+#import "UserDetailsViewController.h"
 
 @interface RepositoryViewController ()
 
@@ -14,9 +15,34 @@
 
 @implementation RepositoryViewController
 
+#pragma mark view life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setLabels];
 }
 
+#pragma mark - user interaction methods
+- (IBAction)ownerDetailsButtonTouched:(UIButton *)sender {
+    UserDetailsViewController *userDetailsViewController = [[UIStoryboard mainStoryboard] instantiateViewControllerWithIdentifier:@"userDetailsID"];
+    userDetailsViewController.author = self.repository.author;
+    [self.navigationController pushViewController:userDetailsViewController animated:YES];
+}
+
+- (IBAction)repositoryWebButtonTouched:(UIButton *)sender {
+    NSDictionary *options = @{UIApplicationOpenURLOptionUniversalLinksOnly : @NO};
+    if ([[UIApplication sharedApplication] canOpenURL:self.repository.detailsUrl]) {
+        [[UIApplication sharedApplication] openURL:self.repository.detailsUrl options:options completionHandler:nil];    }
+}
+
+- (void)setLabels {
+    self.nameLabel.text = self.repository.nameString;
+    self.authorNameLabel.text = self.repository.author.nameString;
+    self.watchersNumberLabel.text = self.repository.watchersString;
+    self.forksNumberLabel.text = self.repository.forkNumberString;
+    self.issueNumberLabel.text = self.repository.issueNumberString;
+    self.programmingLanguageLabel.text = self.repository.languageString;
+    self.dateOfCreationLabel.text = [NSDateFormatter localizedStringFromDate:self.repository.creationDate dateStyle:NSDateFormatterShortStyle timeStyle:NSDateFormatterNoStyle];
+    self.dateOfChangeLabel.text = [NSDateFormatter localizedStringFromDate:self.repository.updateDate dateStyle:NSDateFormatterShortStyle timeStyle:NSDateFormatterNoStyle];
+
+}
 @end
